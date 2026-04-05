71801d05f649a4e503f7b88fd2f099168785e8d3
Owain Lewis <owain@owainlewis.com>
2026-04-05 08:09:49 +0100

    Add Google Analytics tracking to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Bricolage_Grotesque, DM_Sans, Instrument_Serif, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const bricolageGrotesque = Bricolage_Grotesque({
@@ -54,6 +55,20 @@ export default function RootLayout({
       lang="en"
       className={`${bricolageGrotesque.variable} ${dmSans.variable} ${instrumentSerif.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-S76YWXC4XY"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-S76YWXC4XY');
+          `}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col font-sans">
         {children}
       </body>
